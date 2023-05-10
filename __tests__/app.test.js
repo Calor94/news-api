bbee524566a263320312b0af2da0a682de5def28
Callum Neale <callum@Callums-MacBook-Air.local>
2023-05-10 09:59:10 +0100

    updated to include 404 and 500 errors

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,13 +1,36 @@
 const request = require("supertest");
 const app = require("../app");
+const seed = require("../db/seeds/seed");
+const data = require("../db/data/test-data/index");
+const connection = require("../db/connection");
+
+beforeEach(() => {
+  return seed(data);
+});
+
+afterAll(() => {
+  return connection.end();
+});
+
+describe("Invalid endpoint", () => {
+  test("If an endpoint doesn't exist, respond with a 404 and custom message", () => {
+    return request(app)
+      .get("/api/nonsense")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Page not found");
+      });
+  });
+});
 
 describe("/api/topics", () => {
   test("GET /api/topics responds with status 200 and responds with an array of objects", () => {
     return request(app)
       .get("/api/topics")
       .expect(200)
-      .then((res) => {
-        res.body.topics.forEach((topic) => {
+      .then(({ body }) => {
+        expect(body.topics.length).toBe(3);
+        body.topics.forEach((topic) => {
           expect(typeof topic.slug).toBe("string");
           expect(typeof topic.description).toBe("string");
         });
--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require("express");
 const { getTopics } = require("./controllers/get.controllers");
 const app = express();
 
-app.use(express.json());
-
 app.get("/api/topics", getTopics);
+
+app.all("*", (req, res) => {
+  res.status(404).send({ msg: "Page not found" });
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send("Internal Server Error");
+});
 
 module.exports = app;
