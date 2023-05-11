7575154e0a9ca12cbd73520b59fe39a5511742be
Callum Neale <callum@Callums-MacBook-Air.local>
2023-05-11 09:59:20 +0100

    Complete task 4

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -36,7 +36,7 @@ describe("/api", () => {
 });
 
 describe("/api/topics", () => {
-  test("GET /api/topics responds with status 200 and responds with an array of objects", () => {
+  test("GET /api/topics responds with status 200 and an array of objects", () => {
     return request(app)
       .get("/api/topics")
       .expect(200)
@@ -46,6 +46,40 @@ describe("/api/topics", () => {
           expect(typeof topic.slug).toBe("string");
           expect(typeof topic.description).toBe("string");
         });
+      });
+  });
+});
+
+describe("/api/articles/:article_id", () => {
+  test("GET /api/articles/:article_id responds with status 200 and the requested object", () => {
+    return request(app)
+      .get("/api/articles/5")
+      .expect(200)
+      .then(({ body }) => {
+        expect(typeof body.author).toBe("string");
+        expect(typeof body.title).toBe("string");
+        expect(typeof body.article_id).toBe("number");
+        expect(typeof body.body).toBe("string");
+        expect(typeof body.topic).toBe("string");
+        expect(typeof body.created_at).toBe("string");
+        expect(typeof body.votes).toBe("number");
+        expect(typeof body.article_img_url).toBe("string");
+      });
+  });
+  test("GET /api/articles/:article_id responds with status 404 and error message when given a valid article id but doesn't exist", () => {
+    return request(app)
+      .get("/api/articles/72")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("No article found for article_id 72");
+      });
+  });
+  test("GET /api/articles/:article_id responds with status 400 and error message when given an invalid article id", () => {
+    return request(app)
+      .get("/api/articles/invalid")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Invalid input");
       });
   });
 });
--- models/get.models.js
+++ models/get.models.js
@@ -5,3 +5,24 @@ exports.fetchTopics = () => {
     return topics.rows;
   });
 };
+
+exports.fetchArticle = (articleId) => {
+  return connection
+    .query("SELECT * FROM articles WHERE article_id = $1;", [articleId])
+    .then(({ rows }) => {
+      const article = rows[0];
+      if (!article) {
+        return Promise.reject({
+          status: 404,
+          msg: `No article found for article_id ${articleId}`,
+        });
+      }
+      return article;
+    });
+};
+
+// exports.selectSnackById = (snack_id) => {
+//   return db
+//     .query("SELECT * FROM snacks WHERE snack_id = $1;", [snack_id])
+//     .then((result) => result.rows[0]);
+// };
